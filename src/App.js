52eebd2217732d257bfa4f943e0ecdf2d3ae0a06
ArjunK01 <arjunkumar01@gmail.com>
2021-04-03 14:18:48 -0400

    Fetch questions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10").then(res => res.json()).then(res => setQuestions(res.results))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {questions.map(q => <h2>{q.question}</h2>)}
     </div>
   );
 }
